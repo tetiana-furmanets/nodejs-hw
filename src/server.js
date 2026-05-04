2bef7bf1095b666be5f77a0003312e35a6ffedfb
Tetiana Furmanets <tetyana.best.89@ukr.net>
2026-05-04 15:21:48 +0200

    add pino-pretty logger

--- src/server.js
+++ src/server.js
@@ -10,8 +10,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(pino());
-
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
 app.get('/notes', (req, res) => {
   res.status(200).json({
     message: 'Retrieved all notes',
