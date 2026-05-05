a13bc11aeb43fe66c1a113eb9d5a3ee9e759b598
Tetiana Furmanets <tetyana.best.89@ukr.net>
2026-05-05 15:22:53 +0200

    complete homework: express app with middleware and routes

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
+import "dotenv/config";
+import helmet from "helmet"
 import pino from 'pino-http';
-
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+console.log(process.env.PORT);
 app.use(cors());
+app.use(helmet());
 app.use(express.json());
 app.use(
   pino({
